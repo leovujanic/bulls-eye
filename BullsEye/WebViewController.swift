810ed9bf31cdbcc04486d3480b8c63ebad86094b
Leo Vujanic <leonard.vujanic@styria.hr>
2018-10-10 08:05:37 +0200

    Update

---
+++ BullsEye/WebViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WebViewController.swift
+//  BullsEye
+//
+//  Created by Leo Vujanić on 20/08/2018.
+//  Copyright © 2018 Leo Vujanić. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
